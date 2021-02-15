a4e00015e612e30ada516b1c0a4ade3c767d7bd1
Glitch (hello-express) <none>
2021-02-15 14:51:07 +0000

    💒👨‍❤️‍💋‍👨 Checkpoint ./server.js:901636/97

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var server = require('http').createServer();
 var options = {
-  cors: {
-    origin: "https://launch.playcanvas.com",
-    methods: ["GET", "POST"]
-  }
+  cors: true
 }
 
 var io = require('socket.io')(server, options);
